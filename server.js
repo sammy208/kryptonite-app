6492d6b708f4d990ff82e198ac2bebce9f54f892
Ndubuisi Samuel <sammycruiz2008@gmail.com>
2024-06-03 16:03:32 +0100

    Commiting Changes

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ const authRoutes = require('./routes/authRoutes'); // Import authentication rout
 const fileRoutes = require('./routes/fileRoutes'); // Import file handling routes
 
 const app = express(); // Initialize an Express application
+const PORT = process.env.PORT || 5000; // Define the port to run the server on
 
 // Middleware
 app.use(bodyParser.json()); // Use Body Parser middleware to parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Use Body Parser middleware to parse URL-encoded bodies
 
-// Database connection
-mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 60000})
-    .then(() => console.log('MongoDB connected')) // Connect to MongoDB and log success message
-    .catch(err => console.log(err)); // Log any connection errors
 
-// Routes
+//routes
 app.use('/api/auth', authRoutes); // Use authentication routes for /api/auth
 app.use('/api/files', fileRoutes); // Use file routes for /api/files
 
-const PORT = process.env.PORT || 5000; // Define the port to run the server on
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`); // Start the server and log the port it's running on
-});
+
+
+// Database connection
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 60000 })
+  .then(function() {
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`); // Start the server and log the port it's running on
+    });
+    console.log('DB connected...');
+  }) // Connect to MongoDB and log success message
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);//exit the process 
+  }); 
